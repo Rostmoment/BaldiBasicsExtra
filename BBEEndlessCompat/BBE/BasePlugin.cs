5a9f556fba5eae6d3c6ee5078088341b4f1127c4
Rostmoment <110627353+Rostmoment@users.noreply.github.com>
2024-04-22 22:18:51 +0300

    Add files via upload

---
+++ BBEEndlessCompat/BBE/BasePlugin.cs
@@ -0,0 +1,43 @@
+﻿using BepInEx;
+using HarmonyLib;
+using MTM101BaldAPI.SaveSystem;
+using System;
+using BBE.Events;
+using BBE;
+using MTM101BaldAPI.Registers;
+using BaldiEndless;
+using System.Linq;
+using BBE.ExtraContents;
+using UnityEngine;
+using MTM101BaldAPI;
+using Steamworks;
+
+namespace BBEEndless
+{
+    [BepInPlugin("rost.moment.baldiplus.extramod.endlessfloors", "Baldi's Basics Extra Endless Floors Integration", "0.1.1.3")]
+    [BepInDependency("rost.moment.baldiplus.extramod")]
+    [BepInDependency("mtm101.rulerp.baldiplus.endlessfloors")]
+    public class BBEEndless : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Harmony harmony = new Harmony("ost.moment.baldiplus.extramod.endlessfloors");
+            harmony.PatchAll();
+            LoadingEvents.RegisterOnAssetsLoaded(() =>
+            {
+                EndlessFloorsPlugin.AddGeneratorAction(Info, (data) =>
+                {
+                    data.items.Add(new WeightedItemObject() { selection=ItemFromKey("Calculator"), weight=60} );
+                    data.items.Add(new WeightedItemObject() { selection = ItemFromKey("GravityDevice"), weight = 60 });
+                    data.items.Add(new WeightedItemObject() { selection = ItemFromKey("ReductionPotions"), weight = 60 });
+                    data.items.Add(new WeightedItemObject() { selection = ItemFromKey("Shield"), weight = 60 });
+                });
+            }, true);
+        }
+        private ItemObject ItemFromKey(string key)
+        {
+            Items item = EnumExtensions.ExtendEnum<Items>(key);
+            return ItemMetaStorage.Instance.FindByEnum(item).value;
+        }
+    }
+}
