75ea73a79caf4c932d379868f41c7887aa362e84
Rostmoment <110627353+Rostmoment@users.noreply.github.com>
2024-06-14 22:59:28 +0300

    Delete BBE directory

--- BBE/obj/Debug/net46/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace BBE
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "BBE";
-        public const string PLUGIN_NAME = "BBE";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
