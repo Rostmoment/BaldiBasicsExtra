d5a64037a82ee22fd93c6a2bb669c65f6eaa640f
Rostmoment <110627353+Rostmoment@users.noreply.github.com>
2024-07-14 18:31:36 +0300

    Add files via upload

---
+++ BBE/Patches/MoreSubtitles.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HarmonyLib;
+using BBE.Helpers;
+using System.Drawing;
+
+namespace BBE.Patches
+{
+    [HarmonyPatch(typeof(ITM_AlarmClock))]
+    internal class AlarmClockSubtitle
+    {
+        [HarmonyPatch("Clicked")]
+        [HarmonyPrefix]
+        private static void Clicked(ref SoundObject ___audWind)
+        {
+            ___audWind.subDuration = 1f;
+            ___audWind.subtitle = true;
+            ___audWind.soundKey = "*Winding*";
+        }
+    }
+}
